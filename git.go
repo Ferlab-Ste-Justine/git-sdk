4bf946112aabed6e51671639dc1180704258f85b
Eric Vallee <eric_vallee@webificservices.com>
2023-04-04 07:53:17 -0400

    INFRA-216 Fixed ssh error when pushing. Decoupled filesystem requirement for credentials into separate functions. Abstracted away go-git structures so that it is not a direct module requirement to be managed by dependents

--- git.go
+++ git.go
@@ -18,7 +18,32 @@ import (
 	"github.com/ProtonMail/go-crypto/openpgp/packet"
 )
 
-func getPublicKeys(sshKeyPath string, knownHostsPath string) (*ssh.PublicKeys, error) {
+/*
+Structure abstracting away ssh.PublicKeys structure needed by go-git to authenticate with git server
+*/
+type SshCredentials struct {
+	Keys *ssh.PublicKeys
+}
+
+/*
+Structure abstracting away openpgp.Entity structure needed by go-git to sign keys
+*/
+type CommitSignatureKey struct {
+	Entity *openpgp.Entity
+}
+
+/*
+Structure abstracting away gogit.Repository structure needed by go-git to manipulate a git repository
+*/
+type GitRepository struct {
+	Repo *gogit.Repository
+}
+
+/*
+Produces ssh credentials needed by go-git to clone/pull a remote repository and push to it.
+Arguments are file paths to the private ssh key of the user and ssh host key fingerprint of the git server.
+*/
+func GetSshCredentials(sshKeyPath string, knownHostsPath string) (*SshCredentials, error) {
 	_, statErr := os.Stat(sshKeyPath)
 	if statErr != nil {
 		return nil, errors.New(fmt.Sprintf("Failed to access ssh key file %s: %s", sshKeyPath, statErr.Error()))
@@ -41,133 +66,14 @@ func getPublicKeys(sshKeyPath string, knownHostsPath string) (*ssh.PublicKeys, e
 
 	(*publicKeys).HostKeyCallbackHelper.HostKeyCallback = callback
 
-	return publicKeys, nil
-}
-
-func cloneRepo(dir string, url string, ref string, pk *ssh.PublicKeys) (*gogit.Repository, error) {
-	repo, cloneErr := gogit.PlainClone(dir, false, &gogit.CloneOptions{
-		Auth:              pk,
-		RemoteName:        "origin",
-		URL:               url,
-		ReferenceName:     plumbing.NewBranchReferenceName(ref),
-		SingleBranch:      true,
-		NoCheckout:        false,
-		RecurseSubmodules: gogit.NoRecurseSubmodules,
-		Progress:          nil,
-		Tags:              gogit.NoTags,
-	})
-	if cloneErr != nil {
-		return repo, errors.New(fmt.Sprintf("Error cloning in directory \"%s\": %s", dir, cloneErr.Error()))
-	}
-
-	fmt.Println(fmt.Sprintf("Cloned branch \"%s\" of repo \"%s\"", ref, url))
-	return repo, nil
-}
-
-func pullRepo(dir string, url string, ref string, pk *ssh.PublicKeys) (*gogit.Repository, bool, error) {
-	repo, gitErr := gogit.PlainOpen(dir)
-	if gitErr != nil {
-		return repo, true, errors.New(fmt.Sprintf("Error accessing repo in directory \"%s\": %s", dir, gitErr.Error()))
-	}
-
-	worktree, worktreeErr := repo.Worktree()
-	if worktreeErr != nil {
-		return repo, true, errors.New(fmt.Sprintf("Error accessing worktree in directory \"%s\": %s", dir, worktreeErr.Error()))
-	}
-
-	pullErr := worktree.Pull(&gogit.PullOptions{
-		Auth:              pk,
-		RemoteName:        "origin",
-		ReferenceName:     plumbing.NewBranchReferenceName(ref),
-		SingleBranch:      true,
-		RecurseSubmodules: gogit.NoRecurseSubmodules,
-		Progress:          nil,
-		Force:             true,
-	})
-	if pullErr != nil && pullErr.Error() != gogit.NoErrAlreadyUpToDate.Error() {
-		fastForwardProblems := pullErr.Error() == gogit.ErrNonFastForwardUpdate.Error()
-		return repo, fastForwardProblems, errors.New(fmt.Sprintf("Error pulling latest changes in directory \"%s\": %s", dir, pullErr.Error()))
-	}
-	
-	if pullErr != nil && pullErr.Error() == gogit.NoErrAlreadyUpToDate.Error() {
-		fmt.Println(fmt.Sprintf("Branch \"%s\" of repo \"%s\" is up-to-date", ref, url))
-	} else {
-		head, headErr := repo.Head()
-		if headErr != nil {
-			return repo, true, errors.New(fmt.Sprintf("Error accessing top commit in directory \"%s\": %s", dir, headErr.Error()))
-		}
-		fmt.Println(fmt.Sprintf("Branch \"%s\" of repo \"%s\" was updated to commit %s", ref, url, head.Hash()))
-	}
-
-	return repo, false, nil
+	return &SshCredentials{publicKeys}, nil
 }
 
 /*
-Clone or pull the given reference of a given repo at a given path.
-If the repo was previously cloned at the path, a pull will be done, else a clone.
+Produces a commit signature needed to sign a commit.
+Arguments are file paths to an armored private pgp key and optionally a passphrase to decrypt it if it is encrypted
 */
-func SyncGitRepo(dir string, url string, ref string, sshKeyPath string, knownHostsPath string) (*gogit.Repository, bool, error) {
-	pk, pkErr := getPublicKeys(sshKeyPath, knownHostsPath)
-	if pkErr != nil {
-		return nil, false, pkErr
-	}
-
-	_, err := os.Stat(path.Join(dir, ".git"))
-	if err != nil {
-		if !os.IsNotExist(err) {
-			return nil, false, errors.New(fmt.Sprintf("Error accessing repo directory's .git sub-directory: %s", err.Error()))
-		}
-
-		repo, cloneErr := cloneRepo(dir, url, ref, pk)
-		return repo, false, cloneErr
-	}
-
-	return pullRepo(dir, url, ref, pk)
-}
-
-/*
-Verifies that the top commit of a given git repository was signed by one of the keys that are passed in the argument. 
-Returns an error if it isn't.
-*/
-func VerifyTopCommit(repo *gogit.Repository, armoredKeyrings []string) error {
-	head, headErr := repo.Head()
-	if headErr != nil {
-		return errors.New(fmt.Sprintf("Error accessing repo head: %s", headErr.Error()))
-	}
-
-	commit, commitErr := repo.CommitObject(head.Hash())
-	if commitErr != nil {
-		return errors.New(fmt.Sprintf("Error accessing repo top commit: %s", commitErr.Error()))
-	}
-
-	for _, armoredKeyring := range armoredKeyrings {
-		entity, err := commit.Verify(armoredKeyring)
-		if err == nil {
-			for _, identity := range entity.Identities {
-				fmt.Println(fmt.Sprintf("Validated top commit \"%s\" is signed by user \"%s\"", head.Hash(), (*identity).Name))
-			}
-			return nil
-		}
-	}
-
-	return errors.New(fmt.Sprintf("Top commit \"%s\" isn't signed with any of the trusted keys", head.Hash()))
-}
-
-/*
-Optional parameters to pass to the CommitFiles command
-*/
-type CommitOptions struct {
-	//Name of the commiter
-	Name           string
-	//Email of the commiter
-	Email          string
-	//Path of gpg armored private key to sign commit with
-	SignKeyPath    string
-	//Path of optional passphrase if the gpg armored private key is encrypted
-	PassphrasePath string
-}
-
-func getSignKey(signKeyPath string, passphrasePath string) (*openpgp.Entity, error) {
+func GetSignatureKey(signKeyPath string, passphrasePath string) (*CommitSignatureKey, error) {
 	signKey, readSignKeyErr := os.ReadFile(signKeyPath)
 	if readSignKeyErr != nil {
 		return nil, errors.New(fmt.Sprintf("Error reading signing key: %s", readSignKeyErr.Error()))
@@ -204,15 +110,131 @@ func getSignKey(signKeyPath string, passphrasePath string) (*openpgp.Entity, err
 		}
 	}
 
-	return signEntity, nil
+	return &CommitSignatureKey{signEntity}, nil
 } 
+
+func cloneRepo(dir string, url string, ref string, pk *ssh.PublicKeys) (*GitRepository, error) {
+	repo, cloneErr := gogit.PlainClone(dir, false, &gogit.CloneOptions{
+		Auth:              pk,
+		RemoteName:        "origin",
+		URL:               url,
+		ReferenceName:     plumbing.NewBranchReferenceName(ref),
+		SingleBranch:      true,
+		NoCheckout:        false,
+		RecurseSubmodules: gogit.NoRecurseSubmodules,
+		Progress:          nil,
+		Tags:              gogit.NoTags,
+	})
+	if cloneErr != nil {
+		return &GitRepository{repo}, errors.New(fmt.Sprintf("Error cloning in directory \"%s\": %s", dir, cloneErr.Error()))
+	}
+
+	fmt.Println(fmt.Sprintf("Cloned branch \"%s\" of repo \"%s\"", ref, url))
+	return &GitRepository{repo}, nil
+}
+
+func pullRepo(dir string, url string, ref string, pk *ssh.PublicKeys) (*GitRepository, bool, error) {
+	repo, gitErr := gogit.PlainOpen(dir)
+	if gitErr != nil {
+		return &GitRepository{repo}, true, errors.New(fmt.Sprintf("Error accessing repo in directory \"%s\": %s", dir, gitErr.Error()))
+	}
+
+	worktree, worktreeErr := repo.Worktree()
+	if worktreeErr != nil {
+		return &GitRepository{repo}, true, errors.New(fmt.Sprintf("Error accessing worktree in directory \"%s\": %s", dir, worktreeErr.Error()))
+	}
+
+	pullErr := worktree.Pull(&gogit.PullOptions{
+		Auth:              pk,
+		RemoteName:        "origin",
+		ReferenceName:     plumbing.NewBranchReferenceName(ref),
+		SingleBranch:      true,
+		RecurseSubmodules: gogit.NoRecurseSubmodules,
+		Progress:          nil,
+		Force:             true,
+	})
+	if pullErr != nil && pullErr.Error() != gogit.NoErrAlreadyUpToDate.Error() {
+		fastForwardProblems := pullErr.Error() == gogit.ErrNonFastForwardUpdate.Error()
+		return &GitRepository{repo}, fastForwardProblems, errors.New(fmt.Sprintf("Error pulling latest changes in directory \"%s\": %s", dir, pullErr.Error()))
+	}
+	
+	if pullErr != nil && pullErr.Error() == gogit.NoErrAlreadyUpToDate.Error() {
+		fmt.Println(fmt.Sprintf("Branch \"%s\" of repo \"%s\" is up-to-date", ref, url))
+	} else {
+		head, headErr := repo.Head()
+		if headErr != nil {
+			return &GitRepository{repo}, true, errors.New(fmt.Sprintf("Error accessing top commit in directory \"%s\": %s", dir, headErr.Error()))
+		}
+		fmt.Println(fmt.Sprintf("Branch \"%s\" of repo \"%s\" was updated to commit %s", ref, url, head.Hash()))
+	}
+
+	return &GitRepository{repo}, false, nil
+}
+
+/*
+Clone or pull the given reference of a given repo at a given path.
+If the repo was previously cloned at the path, a pull will be done, else a clone.
+*/
+func SyncGitRepo(dir string, url string, ref string, sshCred *SshCredentials) (*GitRepository, bool, error) {
+	_, err := os.Stat(path.Join(dir, ".git"))
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return nil, false, errors.New(fmt.Sprintf("Error accessing repo directory's .git sub-directory: %s", err.Error()))
+		}
+
+		repo, cloneErr := cloneRepo(dir, url, ref, sshCred.Keys)
+		return repo, false, cloneErr
+	}
+
+	return pullRepo(dir, url, ref, sshCred.Keys)
+}
+
+/*
+Verifies that the top commit of a given git repository was signed by one of the keys that are passed in the argument. 
+Returns an error if it isn't.
+*/
+func VerifyTopCommit(repo *GitRepository, armoredKeyrings []string) error {
+	head, headErr := repo.Repo.Head()
+	if headErr != nil {
+		return errors.New(fmt.Sprintf("Error accessing repo head: %s", headErr.Error()))
+	}
+
+	commit, commitErr := repo.Repo.CommitObject(head.Hash())
+	if commitErr != nil {
+		return errors.New(fmt.Sprintf("Error accessing repo top commit: %s", commitErr.Error()))
+	}
+
+	for _, armoredKeyring := range armoredKeyrings {
+		entity, err := commit.Verify(armoredKeyring)
+		if err == nil {
+			for _, identity := range entity.Identities {
+				fmt.Println(fmt.Sprintf("Validated top commit \"%s\" is signed by user \"%s\"", head.Hash(), (*identity).Name))
+			}
+			return nil
+		}
+	}
+
+	return errors.New(fmt.Sprintf("Top commit \"%s\" isn't signed with any of the trusted keys", head.Hash()))
+}
+
+/*
+Optional parameters to pass to the CommitFiles command
+*/
+type CommitOptions struct {
+	//Name of the commiter
+	Name           string
+	//Email of the commiter
+	Email          string
+	//Optional key used to signed the git commit
+	SignatureKey   *CommitSignatureKey
+}
 
 /*
 Commits the given list of files in the git repository.
 If not changes are detected in the files provided, a commit will not be attempted.
 */
-func CommitFiles(repo *gogit.Repository, files []string, msg string, opts CommitOptions) (bool, error) {
-	w, wErr := repo.Worktree()
+func CommitFiles(repo *GitRepository, files []string, msg string, opts CommitOptions) (bool, error) {
+	w, wErr := repo.Repo.Worktree()
 	if wErr != nil {
 		return false, errors.New(fmt.Sprintf("Error accessing repo worktree: %s", wErr.Error()))
 	}
@@ -243,13 +265,8 @@ func CommitFiles(repo *gogit.Repository, files []string, msg string, opts Commit
 		}
 	}
 
-	if opts.SignKeyPath != "" {
-		signEntity, signEntErr := getSignKey(opts.SignKeyPath, opts.PassphrasePath)
-		if signEntErr != nil {
-			return false, signEntErr
-		}
-		
-		comOpts.SignKey = signEntity
+	if opts.SignatureKey != nil {
+		comOpts.SignKey = opts.SignatureKey.Entity
 	}
 
 	_, commErr := w.Commit(msg, &comOpts)
@@ -267,14 +284,14 @@ Function signature meant to be passed as an argument to the PushChanges function
 It should return a git repository with changes to push if there are changes to push otherwise it should return nil.
 The function should be idempotent as it might be called repeatedly if there is a conflict during the push
 */
-type PushPreHook func() (*gogit.Repository, error)
+type PushPreHook func() (*GitRepository, error)
 
 /*
 Takes a function argument that should return a git repository with changes to push if there are (and nil otherwise).
 From there, it will try to push the new commits in the repository to the given reference on origin.
 If there are conflicts during the push, it will keep retrying by re-invoking its function argument and push on the returned repository.
 */
-func PushChanges(hook PushPreHook, ref string, retries int64, retryInterval time.Duration) error {	
+func PushChanges(hook PushPreHook, ref string, sshCred *SshCredentials, retries int64, retryInterval time.Duration) error {
 	repo, hookErr := hook()
 	if hookErr != nil {
 		return hookErr
@@ -286,7 +303,8 @@ func PushChanges(hook PushPreHook, ref string, retries int64, retryInterval time
 	}
 
 	refMap := gogitconf.RefSpec(fmt.Sprintf("refs/heads/%s:refs/heads/%s", ref, ref))
-	pushErr := repo.Push(&gogit.PushOptions{
+	pushErr := repo.Repo.Push(&gogit.PushOptions{
+		Auth: sshCred.Keys,
 		Force: false,
 		Prune: false,
 		RemoteName: "origin",
@@ -307,10 +325,10 @@ func PushChanges(hook PushPreHook, ref string, retries int64, retryInterval time
 			fmt.Println("Push operation failed as remote was updated with non-local commits. Will retry.")
 			time.Sleep(retryInterval)
 
-			return PushChanges(hook, ref, retries - 1, retryInterval)
+			return PushChanges(hook, ref, sshCred, retries - 1, retryInterval)
 		}
 
-		return pushErr
+		return errors.New(fmt.Sprintf("Error pushing file changes: %s", pushErr.Error()))
 	}
 
 	return nil
